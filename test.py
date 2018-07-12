e0db5a48c630c6de26ae1a20f7f67e5cd0838ab5
Stefan Eng <eng_steff@hotmail.com>
2018-07-12 09:43:40 +0200

    Use toml-file to specify dimensions.

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import bpy
 import sys
 import os
+import pytoml
 
 #import pudb; pudb.set_trace()
 
 from bpy import context
 from math import sin, cos, radians
 
-add_cube = bpy.ops.mesh.primitive_cube_add
+with open("dims.toml") as fp:
+    dims = pytoml.load(fp)
+
+def objects():
+    return bpy.data.objects
+
+def add_cube(location=[0,0,0], dim=[1,1,1], layers=[True]+19*[False]):
+    prev_objects = set(objects())
+    bpy.ops.mesh.primitive_cube_add(layers=layers)
+    return list(set(objects()).difference(prev_objects))[0]
+
 layers = [False]*20
 layers[0] = True
 
@@ -26,11 +37,14 @@ for arg in sys.argv:
         OUTPUT_NAME = arg.split('.py')[0]
 
 
-for theta in anglesInRadians:
-    x = cursor.x + radius * cos(theta)
-    y = cursor.y + radius * sin(theta)
-    z = cursor.z
-    add_cube(location=(x, y, z), layers=layers)
+#for theta in anglesInRadians:
+#    x = cursor.x + radius * cos(theta)
+#    y = cursor.y + radius * sin(theta)
+#    z = cursor.z
+#    add_cube(location=(x, y, z), layers=layers)
+
+intersecting_cube = add_cube()
+intersecting_cube.scale = (2,2,2)
 
 
 bpy.context.scene.render.filepath = os.path.join(
@@ -43,3 +57,4 @@ bpy.data.objects['Cube'].scale = (3, 2, 1)
 
 bpy.ops.render.render(write_still = True)
 print(sys.argv)
+print(dims)
